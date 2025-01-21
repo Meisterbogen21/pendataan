b84c8752338ede2ef53e9f235f398f1f347be8be
Meisterbogen21 <setogw1212@gmail.com>
2025-01-21 11:06:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if menu == "Daftarkan Penyewa":
             'Tanggal_Mulai': [tanggal_mulai],
             'Tanggal_Selesai': [tanggal_selesai]
         })
-        global penyewa_data
+        
         penyewa_data = pd.concat([penyewa_data, new_penyewa], ignore_index=True)
 
         st.success("Penyewaan berhasil ditambahkan!")
@@ -78,7 +78,7 @@ if menu == "Daftarkan Mobil Baru":
             'Harga_Sewa': [harga_sewa],
             'Status': [status]
         })
-        global mobil_data
+        
         mobil_data = pd.concat([mobil_data, new_mobil], ignore_index=True)
         st.success("Mobil berhasil ditambahkan!")
 
